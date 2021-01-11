ceea8b8a44d74efdde26dbcb6b5c17b1cd8512b0
Maxim <konstantinovmax@gmail.com>
2021-01-11 13:47:01 +0300

    feat: решить задачу №1

---
+++ index.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const firstQuestion = prompt('Один из последних просмотренных фильмов?', '');
+const secondQuestion = prompt('На сколько оцените его?', '');
+const thirdQuestion = prompt('Один из последних просмотренных фильмов?', '');
+const fourthQuestion = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[firstQuestion] = secondQuestion;
+personalMovieDB.movies[thirdQuestion] = fourthQuestion;
+
+console.log(personalMovieDB);
